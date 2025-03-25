b40c89a2c423225cfa176fe29da7688fc54bd66a
Arpit Singh <asin060902@gmail.com>
2025-03-25 18:39:20 +0530

    minor fix

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -2,6 +2,7 @@ const admin = require("firebase-admin");
 // const serviceAccount = require("../config/serviceAccountKey.json");
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
