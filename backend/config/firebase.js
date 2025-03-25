486d93aa8e1d63199220705d1c0ed5809ed67a4a
Arpit Singh <asin060902@gmail.com>
2025-03-25 18:15:43 +0530

    minor fix for render deployment

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,8 +1,8 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../config/serviceAccountKey.json"); 
+// const serviceAccount = require("../config/serviceAccountKey.json"); 
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(process.env.FIREBASE_SERVICE_ACCOUNT_KEY),
 });
 
 
