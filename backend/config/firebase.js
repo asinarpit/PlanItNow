04081987443a2b4665d54f86fad1295b183e0927
Arpit Singh <asin060902@gmail.com>
2025-03-25 18:18:34 +0530

    minor fixes in backend env

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,9 +1,9 @@
 const admin = require("firebase-admin");
-// const serviceAccount = require("../config/serviceAccountKey.json"); 
+// const serviceAccount = require("../config/serviceAccountKey.json");
 
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 admin.initializeApp({
-  credential: admin.credential.cert(process.env.FIREBASE_SERVICE_ACCOUNT_KEY),
+  credential: admin.credential.cert(serviceAccount),
 });
-
 
 module.exports = admin;
